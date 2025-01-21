1b465c902f98d072539187766a645deab5dab2b3
Pascal7601 <levispascal63@gmail.com>
2025-01-21 21:12:20 +0300

    created a database connection and edited the readme

---
+++ backend/database/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, String, Integer, DateTime, func
+from .connection import Base
+from uuid import uuid4
+
+class BaseModel(Base):
+
+  __abstract__ = True
+
+  id = Column(String(256), primary_key=True, nullable=False, unique=True default=lambda: str(uuid4()))
+  created_at = Column(DateTime, default=func.now())
+  updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
