a856d86b438aea1087481ab5900a7cfbd133a14e
arnoldlupamo@gmail.com <arnoldlupamo@gmail.com>
2025-01-23 21:43:27 +0300

    listings

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
-import Newlistings from './components/Newlistings';
-import { BrowserRouter } from 'react-router-dom';
-// import Routes from './Routes';
+import Home from './pages/Home';
+import Listings from './pages/Listings';
+import { BrowserRouter, Router, Routes, Route } from 'react-router-dom';
+
+
 function App() {
   
  return (
   <BrowserRouter>
-      <Navbar />
-      <Newlistings />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/listings" element={<Listings />} />
+      </Routes>
   </BrowserRouter>
  )
 }
---
+++ frontend/src/pages/Listings.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from '../components/Navbar';
+import Map from '../components/Map';
+import ListingsCards from '../components/ListingsCards';
+
+const Listings = () => {
+	return (
+		<>
+			<Navbar />
+			<h3 style={{color:"#203856", margin:"10px 20px"}}>Listings</h3>
+			<Map />
+			<ListingsCards />
+		</>
+	);
+}
+
+export default Listings;
