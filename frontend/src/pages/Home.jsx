8785e51a618541119d2f7eadee7bbfd05fa086d0
lupamo_Arnold <57684332+lupamo@users.noreply.github.com>
2025-02-02 09:55:16 +0300

    Revert "Eshy"

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,18 +4,8 @@ import Newlistings from '../components/Newlistings';
 import './home.css'
 import HeroSection from '../components/HeroSection';
 import Value from '../components/Value';
-<<<<<<< HEAD
-<<<<<<< HEAD
-import Testimonials from '../components/Testimonials';
-import Footer from '../components/Footer';
-=======
 import Footer from '../components/Footer';
 import Testimonials from '../components/Testimonials';
->>>>>>> a2f3e45f911d387de4af3c8a15b09a964dad910b
-=======
-import Footer from '../components/Footer';
-import Testimonials from '../components/Testimonials';
->>>>>>> a2f3e45f911d387de4af3c8a15b09a964dad910b
 
 const Home = () => {
 	return(
