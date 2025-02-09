8e74f703cd809f0c48f88641d325b949327c64a1
Pascal7601 <levispascal63@gmail.com>
2025-02-09 17:01:48 +0300

    added background tasks config

---
+++ backend/users/models.py
@@ -0,0 +1,20 @@
+from database.models import BaseModel
+from sqlalchemy import Column, String, Integer, Boolean
+from sqlalchemy.orm import relationship
+
+class User(BaseModel):
+  __tablename__ = 'users'
+  
+  username = Column(String(256))
+  email = Column(String(256), nullable=False, unique=True)
+  hashed_passd = Column(String(256), nullable=False, unique=True)
+  contact = Column(String(256))
+  profile_pic = Column(String(256))
+  is_verified = Column(Boolean, default=False)
+
+  # relationships
+  properties = relationship('Property', back_populates='user', cascade="all, delete")
+  bookmarks = relationship('Bookmark', back_populates='user', cascade="all, delete")
+  comments = relationship('Comment', back_populates='user', cascade="all, delete")
+
+  
