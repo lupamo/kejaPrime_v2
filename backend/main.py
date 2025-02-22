089973fd111283cee0f0116e3d60b2a92513d1f2
Pascal7601 <levispascal63@gmail.com>
2025-02-22 14:51:01 +0300

    deploy3

--- backend/main.py
+++ backend/main.py
@@ -60,5 +60,5 @@ async def get_html_docs():
 # Run server only when executed directly (not when imported)
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 
